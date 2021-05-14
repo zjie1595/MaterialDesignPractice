ee1d6876589d520296018cb8b9f647c71fc07503
周杰 <721825417@qq.com>
2021-05-14 13:16:17 +0800

    组件练习结束

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -5,15 +5,26 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AccelerateInterpolator
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.RangeSlider
 import kotlinx.android.synthetic.main.progress_indicators_determine.*
+import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.progress_indicators_determine)
-        startAnim()
+        setContentView(R.layout.text_field_theme)
+
+    }
+
+    private fun setInputDropDownMenu(editText: AutoCompleteTextView) {
+        val items = listOf("Material", "Design", "Components", "Android")
+        val adapter = ArrayAdapter(this, R.layout.list_item, items)
+        (editText as? AutoCompleteTextView)?.setAdapter(adapter)
     }
 
     private fun startAnim() {
@@ -28,6 +39,15 @@ class MainActivity : AppCompatActivity() {
         animator.duration = 1000 * 10
         animator.interpolator = AccelerateInterpolator()
         animator.start()
+    }
+
+    private fun setLabelFormatter(rangeSlider: RangeSlider) {
+        rangeSlider.setLabelFormatter { value ->
+            val format = NumberFormat.getCurrencyInstance()
+            format.maximumFractionDigits = 0
+            format.currency = Currency.getInstance("USD")
+            format.format(value.toDouble())
+        }
     }
 }
 
